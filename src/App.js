148e02c940431c397c54c34f70dd14ac06ccf707
Shawn Pradeep <shawnprdp@gmail.com>
2023-06-02 11:00:08 -0400

    added dark/light mode toggle + remodled header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Social from "./components/Social/Social";
 import Clock from "./components/Clock/Clock";
+import Header from "./components/Header/Header";
+import Switch from "./components/Switch/Switch";
 
 function App() {
+  const LIGHT = "#E8E8E8";
+  const DARK = "#171512";
+
   const [hour, setHours] = useState(new Date().getHours());
-  
+  const [isDark, toggleDark] = useState(hour > 17 || hour < 7);
+
   useEffect(() => {
     setInterval(() => setHours(new Date().getHours()), 1000);
-  })
-  
-  const [isDark] = useState(hour > 17 || hour < 7);
+    // toggleDark(hour > 17 || hour < 7);
+    if (isDark) {
+      document.getElementById("switch").checked = true;
+    } else {
+      document.getElementById("switch").checked = false;
+    }
+  });
 
-  document.body.style.backgroundColor = isDark ? '#171512' : '#E8E8E8';
+  document.body.style.backgroundColor = isDark ? DARK : LIGHT;
+
+  const toggleTheme = () => {
+    if (isDark) {
+      document.getElementById("main").className = "container-light";
+      document.body.style.backgroundColor = LIGHT;
+      toggleDark(false);
+    } else {
+      document.getElementById("main").className = "container-dark";
+      document.body.style.backgroundColor = DARK;
+      toggleDark(true);
+    }
+
+    if (isDark) {
+      document.getElementById("switch").checked = true;
+    } else {
+      document.getElementById("switch").checked = false;
+    }
+  };
 
   return (
-    <div className={isDark ? "container-dark" : "container-light"}>
-      <p className="title">&gt;&nbsp;SHAWNPRADEEP</p>
-      <Clock isDark={isDark}/>
-      <Social isDark={isDark}/>
+    <div className="container">
+      <div className={isDark ? "container-dark" : "container-light"} id="main">
+        {/* <p className="title">&gt;&nbsp;SHAWNPRADEEP</p> */}
+        <Header />
+        <Clock isDark={isDark} />
+        <Social isDark={isDark} />
+      </div>
+      <div className="theme-button">
+        <Switch toggleTheme={toggleTheme} isDark={isDark} />
+      </div>
     </div>
   );
 }
